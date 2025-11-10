33fb187b8748f07abd9792f2c6ab917b352dd4ac
RamizMagdy12 <ramizmagdy452@gmail.com>
2025-11-11 00:59:13 +0200

    AddNavbarAndCoursesCard

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet,RouterLink, Footer, CommonModule, HttpClientModule],
+  imports: [ RouterOutlet,RouterLink, Footer, CommonModule, HttpClientModule,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
