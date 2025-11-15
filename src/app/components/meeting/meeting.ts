0febd92c338f1f8df19d7391973ddf280dc32724
Abdalaa Khaleel <abdalaakhaleel564@gmail.com>
2025-11-15 18:47:07 +0200

    a

---
+++ src/app/components/meeting/meeting.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-meeting',
+  imports: [RouterLink],
+  templateUrl: './meeting.html',
+  styleUrl: './meeting.css',
+})
+export class Meeting {
+
+}
